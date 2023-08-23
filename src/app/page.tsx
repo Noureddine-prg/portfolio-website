26395b3630d22dfc586427bc87d93ae29609b46e
Noureddine Sidi Abed <noureddinesidiabed6k@gmail.com>
2023-08-23 18:37:43 -0400

    Adjusted page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import GreetingCard from "@components/components/LandingCards/greetingCard";
+import {
+  Details,
+  CardTitle,
+  BodyText,
+} from "@components/components/Typography";
+
 import LandingCard from "@components/components/LandingCards/landingCard";
-import Line from "@widgets/widgets/horizontalLine";
-import Link from "next/link";
+import GreetingCard from "@components/components/LandingCards/greetingCard";
 
 export default function Home() {
   return (
     <div className=" flex justify-center w-screen h-screen p-8 bg-[#E9F6FC]">
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-9 w-full h-full auto-rows-fr max-h-screen">
-        {/* Greeting Card */}
         <GreetingCard />
 
         {/* About Card */}
