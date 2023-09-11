de19f34c758b68993161b1d91b6b84e62d141496
Noureddine-prg <noureddinesidiabed6k@gmail.com>
2023-09-11 12:50:26 -0400

    Added more styling to about layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,26 +7,31 @@ export default function About() {
       {/* Bio Section */}
       <div className="flex flex-row w-full h-full">
         {/* Left Side */}
-        <div className="w-4/6">
+        <div className="w-1/2 flex flex-col justify-center">
           <CardTitle> Hi, nice to meet you! </CardTitle>
           <CardTitle> I’m Noureddine </CardTitle>
-          <h1>
-            Throughout my time at New York City College, I explored the world of
-            software development with an excitement that led me beyond class
-            assignments. This journey took me from academic pursuits to
-            real-world applications, leading me to contribute to diverse
-            software projects, spanning individual initiatives to collaborative
-            team challenges. Currently, my expertise lies in <b>front-end</b>{" "}
-            technologies like
-            <b> React.js, React native, and Flutter, </b>complemented by backend
-            prowess in tools such as SQL Database and MongoDB. Outside formal
-            work, I've been mastering agile project management, which I believe
-            is instrumental in today’s dynamic tech landscape.
-          </h1>
+          <div>
+            <h1 className=" max-w-[650px] mt-12">
+              Throughout my time at New York City College, I explored the world
+              of software development with an excitement that led me beyond
+              class assignments. This journey took me from academic pursuits to
+              real-world applications, leading me to contribute to diverse
+              software projects, spanning individual initiatives to
+              collaborative team challenges.
+              <div className="mt-4" />
+              Currently, my expertise lies in <b>front-end</b> technologies like{" "}
+              <b> React.js, React native, and Flutter, </b>complemented by
+              backend prowess in tools such as SQL Database and MongoDB.
+              <br />
+              <div className="mt-4" />
+              Outside formal work, I've been mastering agile project management,
+              which I believe is instrumental in today’s dynamic tech landscape.
+            </h1>
+          </div>
         </div>
         {/* Right Side */}
-        <div className="w-2/6">
-          <div className="h-full w-full bg-white rounded-3xl"></div>
+        <div className="w-1/2 flex flex-col justify-center">
+          <div className="h-5/6 max-w-md bg-white rounded-3xl"></div>
         </div>
       </div>
 
