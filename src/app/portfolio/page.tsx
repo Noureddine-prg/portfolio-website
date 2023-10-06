508fc76b90ab4b6966b1f6aeee5d7958282880e0
Noureddine Sidi <noureddinesidiabed6k@gmail.com>
2023-10-05 20:11:25 -0400

    Created new branch for the project section layout

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,7 +6,22 @@ import Wrapper from "@components/components/Wrapper"
 export default function Portfolio() {
   return(
   <Wrapper bgColor="bg-[var(--portfolio)]">
+    
     <Navbar/>
+
+    <div className="w-full h-full flex flex-row">
+
+      <div className="w-2/5 h-screen bg-white">
+
+      </div>
+
+      <div className="w-3/5 h-screen bg-green-50 border-r-black">
+        
+      </div>
+
+    </div>
+  
+    
 
   </Wrapper>);
 }
